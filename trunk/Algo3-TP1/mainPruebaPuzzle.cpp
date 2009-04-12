52b403ac07c3180b7e05750564bc83f8d491f97a
matiasgd <matiasgd@fa56611c-26b7-11de-b054-05e72bb6a586>
2009-04-12 20:55:26 +0000

    El main de prueba para el ej 2, cuando este la poda solo hay que cambiar un include

---
+++ trunk/Algo3-TP1/mainPruebaPuzzle.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "puzzleSinPoda.cpp"
+#include "Archivo.h"
+#include <sys/time.h>
+
+using namespace std;
+
+void generarTableroConSolucion (Ficha* fichas, int n) {
+	srand ( time(NULL) );
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			fichas[(i*n)+j].der = rand()%200;
+			fichas[(i*n)+j].abajo = rand()%200;
+
+			if(j == 0) {
+				fichas[(i*n)+j].izq = rand()%200;
+			} else {
+				fichas[(i*n)+j].izq = fichas[(i*n)+j-1].der;
+			}
+
+			if(i == 0) {
+				fichas[(i*n)+j].arriba = rand()%200;
+			} else {
+				fichas[(i*n)+j].arriba = fichas[((i-1)*n)+j].abajo;
+			}
+		}
+	}
+
+	//Ordeno las fichas por su coordenada abajo para mezclarlas
+	int maximo;
+	for(int i = 0; i < n*n; i++) {
+		maximo = i;
+		for(int j = i; j < n*n; j++) {
+			if(fichas[j].abajo > fichas[maximo].abajo) {
+				maximo = j;
+			}
+		}
+
+		Ficha aux;
+		aux.arriba = fichas[i].arriba;
+		aux.abajo = fichas[i].abajo;
+		aux.der = fichas[i].der;
+		aux.izq = fichas[i].izq;
+		
+		fichas[i].arriba = fichas[maximo].arriba;
+		fichas[i].abajo = fichas[maximo].abajo;
+		fichas[i].der = fichas[maximo].der;
+		fichas[i].izq = fichas[maximo].izq;
+
+		fichas[maximo].arriba = aux.arriba;
+		fichas[maximo].abajo = aux.abajo;
+		fichas[maximo].der = aux.der;
+		fichas[maximo].izq = aux.izq;
+	}
+}
+
+int main() {
+
+	for(int n = 1; n <= 16; n++) {
+		Ficha x[n*n];
+		timeval inicio;
+		timeval fin;
+		int tiempo;
+		Archivo salida("Ej2-Complejidad.txt");
+
+		generarTableroConSolucion(x,n);
+
+		gettimeofday(&inicio, NULL);
+		Ficha** resultado = resolverPuzzle(x,n);
+		gettimeofday(&fin, NULL);
+		tiempo = fin.tv_sec - inicio.tv_sec;
+
+// 		cout << "RESULTADO PARA TABLERO " << n << "x" << n <<endl;
+// 		cout << "--------------------------" << endl;
+// 
+// 		for(int i = 0; i < n; i++) {
+// 			for(int j = 0; j < n; j++) {
+// 				cout << resultado[i][j].arriba << " ";
+// 				cout << resultado[i][j].abajo << " ";
+// 				cout << resultado[i][j].izq << " ";
+// 				cout << resultado[i][j].der << " ";
+// 				cout << endl;
+// 			}
+// 		}
+		salida.escribirDato(n);
+		salida.escribirDato(" ");
+		salida.escribirDato(tiempo);
+		salida.nuevaLinea();
+
+		for(int i = 0; i < n; i++)
+			delete resultado[i];
+		
+		delete resultado;
+		
+	}
+	return 0;
+}
