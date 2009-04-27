ff160b1dda7363320b6d621e0fde86d7ee349013
lucianolev <lucianolev@fa56611c-26b7-11de-b054-05e72bb6a586>
2009-04-27 05:38:41 +0000

    Ej1: Mejoras a MainDengue.java

--- Algo3-TP2/MainDengue.java
+++ Algo3-TP2/MainDengue.java
@@ -1,43 +1,64 @@
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 class MainDengue {
 
 	public static void main(String[] args) throws IOException {
-		Dengue dengue = new Dengue();
+		System.out.println("Modo de Uso: java MainDengue (-in <archivoentrada>) || (-random <limite> <esparcimiento> [-fijarparametro <cantidad> (litros || zonas)]) [-out <archivosalida>] [-guardartiempos <archivosalida>]");
+		System.out.println();
 
-		int index;
-		if((index = Arrays.binarySearch(args, "-test")) >= 0) {
-			try {
-				int limite = Integer.parseInt(args[index+1]);
-				int esparcimiento = Integer.parseInt(args[index+2]);
-				if((index = Arrays.binarySearch(args, "-fijarzonas")) >= 0) {
-					int zonas = Integer.parseInt(args[index+1]);
-					dengue.cargarInstanciasRandomConZonasFijoHasta(zonas, limite, esparcimiento);
-				} else if ((index = Arrays.binarySearch(args, "-fijarlitros")) >= 0) {
-					int litros = Integer.parseInt(args[index+1]);
-					dengue.cargarInstanciasRandomConLitrosFijoHasta(litros, limite, esparcimiento);
+		Dengue dengue = new Dengue();
+		ArrayList argslist = new ArrayList(args.length);
+		for(int i = 0; i < args.length; i++) { 
+			argslist.add(args[i]);
+		}
+
+		int indexIn;
+		int indexRandom;
+		int indexFijarParametro;
+		int indexOut;
+		int indexGuardarTiempos;
+		try {
+			if((indexIn = argslist.indexOf("-in")) >= 0) {
+				dengue.cargarInstanciasDeArchivo(args[indexIn+1]);
+				dengue.resolverInstanciasCargadas();
+			} else if((indexRandom = argslist.indexOf("-random")) >= 0) {
+				int limite = Integer.parseInt(args[indexRandom+1]);
+				int esparcimiento = Integer.parseInt(args[indexRandom+2]);
+				if((indexFijarParametro = argslist.indexOf("-fijarparametro")) >= 0) {
+					int cantidad = Integer.parseInt(args[indexFijarParametro+1]);
+					if (args[indexFijarParametro+2].equals("litros")) {
+						dengue.cargarInstanciasRandomConLitrosFijoHasta(cantidad, limite, esparcimiento);
+					}
+					else if (args[indexFijarParametro+2].equals("zonas")) {
+						dengue.cargarInstanciasRandomConZonasFijoHasta(cantidad, limite, esparcimiento);
+					} else {
+						ArrayIndexOutOfBoundsException e = new ArrayIndexOutOfBoundsException();
+						throw e;
+					}
 				} else {
 					dengue.cargarInstanciasRandomHasta(limite, esparcimiento);
 				}
 				dengue.resolverInstanciasCargadas();
-			} catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("Parametros incorrectos!");
-				System.out.println("Modo de Uso:");
-				System.out.println("java MainDengue [-noguardarresultados] [-guardartiempos] [-test <limite> <esparcimiento> [-fijarzonas <zonas>] [-fijarlitros <litros>] ]  ");
-				return;
 			}
-		} else {
-			dengue.cargarInstanciasDeArchivo("Tp2Ej1.in");
-			dengue.resolverInstanciasCargadas();
-		}
 
-		if(!(Arrays.binarySearch(args, "-noguardarresultados") >= 0)) {
-			dengue.guardarResultados("Tp2Ej1.out");
-		}
+			if((indexOut = argslist.indexOf("-out")) >= 0) {
+				dengue.guardarResultados(args[indexOut+1]);
+			}
 
-		if(Arrays.binarySearch(args, "-guardartiempos") >= 0) {
-			dengue.guardarTiemposDeFumigacion("Tp2Ej1-TiemposFumigacion.out");
+			if((indexGuardarTiempos = argslist.indexOf("-guardartiempos")) >= 0) {
+				dengue.guardarTiemposDeFumigacion(args[indexGuardarTiempos+1]);
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Parametros incorrectos!");
+			return;
+		} catch (NumberFormatException e) {
+			System.out.println("Parametros incorrectos!");
+			return;
+		} catch (FileNotFoundException e) {
+			System.out.println("No se encuentra el archivo de entrada!");
+			return;
 		}
 
 	}
