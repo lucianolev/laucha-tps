5ee9d8697f7ec8477e0e2c046a33d8800bff63da
lucianolev <lucianolev@fa56611c-26b7-11de-b054-05e72bb6a586>
2009-04-27 04:35:44 +0000

    Ej1: Opcion para testear y calcular tiempos. Graficos de resultados.

--- trunk/Algo3-TP2/InstanciaDengue.java
+++ trunk/Algo3-TP2/InstanciaDengue.java
@@ -1,17 +1,37 @@
+import java.lang.Math;
+
 public class InstanciaDengue {
 
 	public InstanciaDengue() {
 		zonas = 0;
 		litros = 0;
 		mosquitosMuertos = null;
-		maxMosquitosMuertos = 0;
+		resuelta = false;
 		litrosPorZona = null;
+		tiempoDeFumigacion = 0;
+		tiempoDeLitrosPorZona = 0;
+		maxMosquitosMuertos = 0;
+	}
+
+	public void generarInstanciaRandom(int paramZonas, int paramLitros) {
+		zonas = paramZonas;
+		litros = paramLitros;
+		mosquitosMuertos = new int[zonas][litros+1];
+		for (int i = 0; i < paramZonas; i++) {
+			mosquitosMuertos[i][0] = 0; //el litro 0 es 0 para cada zona
+			for (int j = 1; j <= paramLitros; j++) {
+				mosquitosMuertos[i][j] = (int)(Math.random()*100);
+			}
+		}
 	}
 
 	public int zonas;
 	public int litros;
 	public int[][] mosquitosMuertos;
+	public boolean resuelta;
 	public int maxMosquitosMuertos;
 	public int[] litrosPorZona;
+	public long tiempoDeFumigacion;
+	public long tiempoDeLitrosPorZona;
 
 }
