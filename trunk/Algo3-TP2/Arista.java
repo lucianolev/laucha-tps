297a1b327838477fddbe057661b21a1182fd7c59
matiasgd <matiasgd@fa56611c-26b7-11de-b054-05e72bb6a586>
2009-05-08 00:58:33 +0000

    y lucho tiene mieeeeeeeeeedo

---
+++ trunk/Algo3-TP2/Arista.java
@@ -0,0 +1,14 @@
+class Arista {
+	public int nodo1;
+	public int nodo2;
+	public int peso;
+
+	public Arista() {
+	}
+	public Arista(int p_nodo1, int p_nodo2, int p_peso) {
+		nodo1 = p_nodo1;
+		nodo2 = p_nodo2;
+		peso = p_peso;
+	}
+
+}
---
+++ trunk/Algo3-TP2/AristaComparator.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+
+class AristaComparator implements Comparator {
+	public int compare(Object arista1, Object arista2) {
+
+		if(((Arista)arista1).peso < ((Arista)arista2).peso) {
+			return (-1);
+		}
+
+		if(((Arista)arista1).peso > ((Arista)arista2).peso) {
+			return 1;
+		}
+
+		if(((Arista)arista1).peso == ((Arista)arista2).peso) {
+			return 0;
+		}
+	}
+}
