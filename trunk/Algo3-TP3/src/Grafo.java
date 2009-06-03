9555ddba635d7a1342e9d7dd17a7289a3cfa5955
lucianolev <lucianolev@fa56611c-26b7-11de-b054-05e72bb6a586>
2009-06-03 20:20:26 +0000

    Un poco mas del tp3

--- trunk/Algo3-TP3/src/Grafo.java
+++ trunk/Algo3-TP3/src/Grafo.java
@@ -1,13 +1,45 @@
 import java.util.*;
 
-public class Grafo {
-	
-	public Grafo() {
-		
+public class GrafoNPonderados {
+
+	public GrafoNPonderados(LinkedList[] pListaDeAdyacencias, int pCantNodos, int[] pPesoNodos) {
+		listaDeAdyacencias = pListaDeAdyacencias;
+		cantNodos = pCantNodos;
+		pesoNodos = pPesoNodos;
+		armarMatrizAdyacencias();
+	}
+
+	private void armarMatrizAdyacencias() {
+		matrizAdyacencias = new boolean[cantNodos+1][cantNodos+1];
+		for (int i = 1; i <= cantNodos; i++) {
+			for (int j = 1; j <= cantNodos; j++) {
+				matrizAdyacencias[i][j] = false;
+			}
+		}
+		for (int i = 1; i <= cantNodos; i++) {
+			ListIterator iter = listaDeAdyacencias[i].listIterator();
+			while (iter.hasNext()) {
+				matrizAdyacencias[i][iter.next()] = true;
+			}
+		}
+	}
+
+	public boolean sonAdyacentes(int nodo1, int nodo2) { 
+		return matrizAdyacencias[nodo1][nodo2];	
+	}
+
+	public int pesoNodo(int nodo) {
+		return pesoNodos[nodo];
+	}
+
+	public int cantNodos() {
+		return cantNodos;
 	}
 	
 	private boolean[][] matrizAdyacencias;
-	private LinkedList[] listaDeAdyacencias;
+	private LinkedList[] listaDeAdyacencias;		Grafo elgrafo = lector.dameGrafo();
+
+	private int[] pesoNodos;
 	private int cantNodos;
-	
+
 }
